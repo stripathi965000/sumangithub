d3a9c9fae6bc45a44a93ea3be0689c53ea3a020f
manimittal <mittalmani07@gmail.com>
2026-02-18 15:35:01 +0530

    code pushed

--- RG with for each by satya/main.tf
+++ RG with for each by satya/main.tf
@@ -1,10 +1,31 @@
 #for_each + list
 #for each list support nhi krta isiliye set m covert kia
-resource "azurerm_resource_group" "rg" { 
-    for_each = toset(var.name)
-    name = each.value    #each.value/each.key
-  location = "central india"
-}
-variable "name" {
+# for each support set of string and map
+
+# resource "azurerm_resource_group" "rg" { 
+#     for_each = toset(var.name)
+#     name = each.value    #each.value/each.key
+#   location = "central india"
+# }
+# variable "name" {
   
-}
+# }
+
+# #for_each + MAP (mutliple resource with diff location)
+# map only take key value pair thats y nested map came
+# resource "azurerm_resource_group" "rg" { 
+#     for_each = var.rgs
+#     name = each.key
+#   location = each.value
+# }
+
+# variable "rgs" {}
+
+#for_each + nested MAP (mutliple resource with diff location)
+# resource "azurerm_resource_group" "rg" { 
+#   for_each = var.rgs
+#   name = each.value.name
+#   location = each.value.location
+# }
+
+# variable "rgs" {}
