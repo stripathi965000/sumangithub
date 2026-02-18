0ce2e608dd5c697f2fe162aa0baa8fb641520e68
stripathi965000 <sumantripathi.kpw@gmail.com>
2026-02-18 19:16:21 +0530

    Delete 14 Modular structure with variables directory

--- 14 Modular structure with variables/env/dev/main.tf
+++
@@ -1,9 +0,0 @@
-#1 module "resource_group" {
-#    source =  "../../modules/Resource_Group"
-#    rgs = var.rgs
-# }
-
-module "resource_group" {
-   source =  "../../modules/Resource_Group"
-   rgs = var.rgs
-}
