0ff1193b79b70a8b333c1d7b8317b08ccc41d976
manimittal <mittalmani07@gmail.com>
2026-03-12 13:40:33 +0530

    code addded

--- 17 TO DOapp by satya/6nsg.tf
+++ 17 TO DOapp by satya/6nsg.tf
@@ -21,6 +21,9 @@ resource "azurerm_network_security_group" "nsg" {
     environment = "Production"
   }
 }
+
+#we need to create association between subnet and nsg, for that we need to get the id of subnet and nsg, for that we need to create data source for both subnet and nsg, then we can use the id of both to create association
+
 resource "azurerm_subnet_network_security_group_association" "as" {
   for_each = var.nsgs
   subnet_id                 = data.azurerm_subnet.data_subnet1[each.key].id
@@ -33,6 +36,8 @@ data "azurerm_subnet" "data_subnet1" {
   virtual_network_name = each.value.virtual_network_name
   resource_group_name  = each.value.resource_group_name
 }
+#same way we can create association between nic and nsg, for that we need to create data source for both nic and nsg, then we can use the id of both to create association
+#asg = association security group, we can create association between nic and asg, for that we need to create data source for both nic and asg, then we can use the id of both to create association
 
 data "azurerm_network_security_group" "asg" {
     depends_on = [ azurerm_resource_group.rgs, azurerm_network_security_group.nsg ]
--- 17 TO DOapp by satya/7vm.tf
+++ 17 TO DOapp by satya/7vm.tf
@@ -39,3 +39,97 @@ data "azurerm_network_interface" "data-nic" {
   resource_group_name = each.value.resource_group_name
 }
 
+
+
+# commands to setup nginx web server in the VM and application stabucks  using local laptop as source and vm as destination
+#code directly clone to vm 
+
+#PS C:\Users\hp\Desktop\Suman GitHub DevOps\sumangithub\17 TO DOapp by satya> ssh adminuser@4.247.141.191
+#adminuser@todovm-be:~$ sudo apt update
+#adminuser@todovm-be:~$ sudo apt install nginx -y
+#adminuser@todovm-be:~$ sudo systemctl start nginx
+#adminuser@todovm-be:/var/www/html$ sudo rm -rf *
+#adminuser@todovm-be:/var/www/html$ sudo rm -rf *
+#adminuser@todovm-be:/var/www/html$ ls
+#adminuser@todovm-be:/var/www/html$ 
+#sudo git clone https://github.com/devopsinsiders/starbucks-clone.git
+#adminuser@todovm-be:/var/www/html$ ls
+#starbucks-clone
+#adminuser@todovm-be:/var/www/html$ cd starbucks-clone/
+#adminuser@todovm-be:/var/www/html/starbucks-clone$ ls
+#assets  index.html
+#adminuser@todovm-be:/var/www/html/starbucks-clone$ sudo cp -r * /var/www/html
+#adminuser@todovm-be:/var/www/html/starbucks-clone$ cd ..
+#adminuser@todovm-be:/var/www/html$ ls
+#assets  index.html  starbucks-clone
+#adminuser@todovm-be:/var/www/html$ exit
+
+# commands to setup nginx web server in the VM and application netflix clone using local copy and git clone
+#cloned to local pc then to vm
+# copy code to local pc path 
+#go to vs code and run below command
+
+#PS C:\Users\hp\Desktop\Suman GitHub DevOps\sumangithub\17 TO DOapp by satya> ssh adminuser@4.247.141.191
+#adminuser@todovm-be:~$ sudo apt update
+#adminuser@todovm-be:~$ sudo apt install nginx -y
+#adminuser@todovm-be:~$ sudo systemctl start nginx
+
+#adminuser@todovm-be:~$ cd var/www/html if directly dont come then use below command
+#adminuser@todovm-be:~$ cd /var
+#adminuser@todovm-be:/var$ cd www
+#adminuser@todovm-be:/var/www$ cd html
+#adminuser@todovm-be:/var/www/html$ ls
+ #index.nginx-debian.html
+#adminuser@todovm-be:/var/www/html$ pwd
+ #/var/www/html
+
+#aftercopy in tmp it show here:see below steps in previous terminal
+#adminuser@todovm-be:/tmp/netflix$ ls
+  #README.md  assets  avatar.png  favicon.ico  index.html  logo192.png  logo512.png  manifest.json  robots.txt
+
+
+#in another terminal 
+#PS C:\Users\hp\Desktop\Suman GitHub DevOps\sumangithub> ls
+ #d-----        12-03-2026     10:22                17 TO DOapp by satya
+ #d-----        26-02-2026     15:39                18 TODOAPP modular by satya
+ #d-----        12-03-2026     10:47                StreamFlix
+
+#PS C:\Users\hp\Desktop\Suman GitHub DevOps\sumangithub> cd .\StreamFlix\
+#PS C:\Users\hp\Desktop\Suman GitHub DevOps\sumangithub\StreamFlix> ls 
+ # Directory: C:\Users\hp\Desktop\Suman GitHub DevOps\sumangithub\StreamFlix
+
+
+#Mode                 LastWriteTime         Length Name
+#----                 -------------         ------ ----
+#d-----        12-03-2026     10:47                assets
+#-a----        12-03-2026     10:47           1330 avatar.png
+#-a----        12-03-2026     10:47          15406 favicon.ico
+#-a----        12-03-2026     10:47            816 index.html
+#-a----        12-03-2026     10:47           9707 logo192.png
+#-a----        12-03-2026     10:47          37636 logo512.png
+#-a----        12-03-2026     10:47            517 manifest.json
+#-a----        12-03-2026     10:47           1277 README.md
+#-a----        12-03-2026     10:47             70 robots.txt
+
+#adminuser@todovm-be:/tmp/netflix$ scp -r * adminuser@4.247.141.191:/var/www/html
+
+#we will be getting error as permission denied bcoz we used sudo, for that we will copy in temp file
+#go back to previous terminal
+
+#adminuser@todovm-be:/$ ls
+  #bin  boot  dev  etc  home  lib  lib64  media  mnt  opt  proc  root  run  sbin  srv  sys  tmp  usr  var
+#adminuser@todovm-be:/$ cd tmp
+#adminuser@todovm-be:/tmp$ mkdir netflix
+#adminuser@todovm-be:/tmp$ cd netflix
+#adminuser@todovm-be:/tmp/netflix$ pwd
+  #/tmp/netflix
+# adminuser@todovm-be:/tmp/netflix$ ls
+  #README.md  assets  avatar.png  favicon.ico  index.html  logo192.png  logo512.png  manifest.json  robots.txt
+
+#adminuser@todovm-be:/tmp/netflix$ sudo cp -r * /var/www/html
+#adminuser@todovm-be:/tmp/netflix$ cd /var/www/html
+#adminuser@todovm-be:/var/www/html$ ls
+  #README.md  assets  avatar.png  favicon.ico  index.html  logo192.png  logo512.png  manifest.json  robots.txt
+
+  #it might not show netflix but logo will come
+
